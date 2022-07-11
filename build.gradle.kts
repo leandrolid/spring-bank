e476249b77ced8406394963b17ab6f222632fb43
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-07-10 21:58:55 -0300

    add Procfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
+	implementation("org.springframework.boot:spring-boot-starter")
+
 	// https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-ui
 	implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
 
